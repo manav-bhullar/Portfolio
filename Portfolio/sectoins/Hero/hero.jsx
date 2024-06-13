bf4e016e894ebba9a1a6a13a6a7cf89cad0f52b5
NEW-BORN <manavbhullar341@gmail.com>
2024-06-13 20:39:12 +0530

    chore: Remove unnecessary files and configurations

---
+++ Portfolio/sectoins/Hero/hero.jsx
@@ -0,0 +1,57 @@
+/* eslint-disable no-unused-vars */
+
+import styles from './HeroStyles.module.css'
+import heroImg from '/Users/manav/Library/CloudStorage/OneDrive-thapar.edu/Codes/Portfolio/Portfolio/src/assets/heroImg.png'
+import themeIcon from '/Users/manav/Library/CloudStorage/OneDrive-thapar.edu/Codes/Portfolio/Portfolio/src/assets/sun.svg'
+import xIcon from '/Users/manav/Library/CloudStorage/OneDrive-thapar.edu/Codes/Portfolio/Portfolio/src/assets/twitter-light.svg'
+import linkedinIcon from '/Users/manav/Library/CloudStorage/OneDrive-thapar.edu/Codes/Portfolio/Portfolio/src/assets/linkedin-light.svg'
+import githubIcon from '/Users/manav/Library/CloudStorage/OneDrive-thapar.edu/Codes/Portfolio/Portfolio/src/assets/github-light.svg'
+import CV from '/Users/manav/Library/CloudStorage/OneDrive-thapar.edu/Codes/Portfolio/Portfolio/src/assets/cv.pdf'
+
+function Hero() {
+    return (
+        <section id='hero' className={styles.container}>
+            <div className={styles.colorModeContainer}>
+                <img 
+                className={styles.hero} 
+                src={heroImg} 
+                alt="Profic Pic" />
+
+                <img src={themeIcon} alt="Dark mode icon" />
+                
+            </div>
+
+            <div className={styles.info}>
+                <h1>
+                    Manav
+                    <br />
+                    Bhullar
+                </h1>
+                <h2>Full stack Web developer</h2>
+                <span>
+                    <a href="https://x.com/ManavBhullar8" target='_blank'>
+                        <img src={xIcon} alt="Twitter Icon" />
+                    </a>
+
+                    <a href="https://www.linkedin.com/in/manav-bhullar-a27a0b282" target='_blank'>
+                        <img src={linkedinIcon} alt="Linkedin Icon" />
+                    </a>
+
+                    <a href="https://github.com/manav-bhullar" target='_blank'>
+                        <img src={githubIcon} alt="Github Icon" />
+                    </a>
+                </span>
+
+                <p>
+                    I am MANAV a Full stack web developer and a tech enthusiast.
+                </p>
+
+                <a href={CV} target='_blank'>
+                    <button className='hover'>Resume</button>
+                </a>
+            </div>
+        </section>
+    );
+}
+
+export default Hero;
---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Hero from '../sectoins/Hero/hero'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Hero />
+    </>
+  )
+}
+
+export default App
