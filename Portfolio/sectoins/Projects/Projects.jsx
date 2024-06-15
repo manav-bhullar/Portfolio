69594704ea61b88fffeebbee7752e9fddd0509bc
NEW-BORN <manavbhullar341@gmail.com>
2024-06-15 14:02:12 +0530

    feat: Add Projects section to App component

---
+++ Portfolio/sectoins/Projects/Projects.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "/Users/manav/Library/CloudStorage/OneDrive-thapar.edu/Codes/Portfolio/Portfolio/sectoins/Projects/ProjectsStyles.module.css"
+
+function Projects() {
+    return(
+        <sections id='projects' className={styles.container}>
+            <h1 >Projects</h1>
+        </sections>
+    )
+}
+
+export default Projects;
--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,6 +1,8 @@
 
 import './App.css'
 import Hero from '../sectoins/Hero/hero'
+import Projects from '../sectoins/Projects/Projects'
+
 
 function App() {
  
@@ -8,6 +10,7 @@ function App() {
   return (
     <>
     <Hero />
+    <Projects />
     </>
   )
 }
